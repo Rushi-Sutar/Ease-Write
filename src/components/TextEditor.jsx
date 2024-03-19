013e5f36e45fba5490c4ac13c983d239696f569e
Rushi-Sutar <rushisutar999@gmail.com>
2024-03-19 16:32:04 +0530

    Button position updated

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -47,7 +47,7 @@ const TextEditor = () => {
      
       </div>
        <button
-       className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+       className="mt-20 z-50 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
        onClick={exportAsDoc}
      >
        Export as DOC
